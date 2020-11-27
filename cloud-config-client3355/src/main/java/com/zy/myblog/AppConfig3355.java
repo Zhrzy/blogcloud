58929d8b18aa95573196899e88e31f83b1dd8b73
Zhrzy <1716457206@qq.com>
2020-11-27 12:43:30 +0800

    配置中心

---
+++ cloud-config-client3355/src/main/java/com/zy/myblog/AppConfig3355.java
@@ -0,0 +1,14 @@
+package com.zy.myblog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AppConfig3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig3355.class, args);
+    }
+}
