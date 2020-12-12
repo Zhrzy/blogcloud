56a8fce0db612b394e65ade12a72afeb23135fb6
Zhrzy <zhanghaorenzhr@163.com>
2020-12-12 14:41:18 +0800

    hystrix

--- cloudalibaba-sentinel-service8402/src/main/java/com/zy/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8402/src/main/java/com/zy/alibaba/controller/FlowLimitController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @auther zzyy
  * @create 2020-02-24 16:26
@@ -22,8 +24,28 @@ public class FlowLimitController
     @GetMapping("/testB")
     public String testB()
     {
-        //log.info(Thread.currentThread().getName()+"\t"+"...testB");
+        System.out.println(Thread.currentThread().getName()+"\t"+"...testB");
         return "------testB";
+    }
+
+    @GetMapping("/testC")
+    public String testC()
+    {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName()+"\t"+"...testC");
+        return "------testC";
+    }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        int a=1/0;
+        System.out.println(Thread.currentThread().getName()+"\t"+"...testD");
+        return "------testD";
     }
 
 
